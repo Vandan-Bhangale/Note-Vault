11566a6b5bd3511c1849ea18d01a4c682fe025d5
Vandan Bhangale <159510950+Vandan-Bhangale@users.noreply.github.com>
2025-08-10 10:24:49 +0530

    Update Register.jsx

--- Frontend/Note-Taking/src/components/Register.jsx
+++ Frontend/Note-Taking/src/components/Register.jsx
@@ -12,7 +12,7 @@ const Register = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await axios.post("http://localhost:3000/api/register", {
+      const response = await axios.post("https://note-vault-1-z260.onrender.com/api/register", {
         name,
         email,
         password
