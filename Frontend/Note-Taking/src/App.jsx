41e4fa332b85453cb5053f9ea7f9d465c3377208
Vandan-Bhangale <vandan9804@gmail.com>
2025-08-19 10:32:39 +0530

    Updated App.jsx file

--- Frontend/Note-Taking/src/App.jsx
+++ Frontend/Note-Taking/src/App.jsx
@@ -24,7 +24,7 @@ function App() {
   useEffect(() => {
     const checkAuthStatus = async () => {
       try {
-        const response = await fetch(`${import.meta.env.VITE_API_URL}/api/status`, {
+        const response = await fetch(`${import.meta.env.VITE_GENERAL_URL}/api/status`, {
           method: "GET",
           credentials: "include",
         });
