3175efd914ef773841596b7cae648dca151ff8e0
Vandan-Bhangale <vandan9804@gmail.com>
2025-08-26 11:12:49 +0530

    Updated code

--- Backend/index.js
+++ Backend/index.js
@@ -19,7 +19,15 @@ const store = new mognodbStore({
 })
 
 //Local modules
-app.use(cors({ origin: process.env.CORS_ORIGIN, credentials: true }));
+// app.use(cors({ origin: process.env.CORS_ORIGIN, credentials: true }));
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 app.use(express.json());
 app.use(cookieParser());
 const PORT = 3000;
