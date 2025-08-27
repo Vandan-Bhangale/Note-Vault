b225970532d6f40f497aaa46c753543e2dcb1067
Vandan-Bhangale <vandan9804@gmail.com>
2025-08-27 11:05:41 +0530

    Updated note model for time stamp feature

--- Backend/model/noteModel.js
+++ Backend/model/noteModel.js
@@ -14,6 +14,10 @@ const noteSchema = new mongoose.Schema({
         type: [String],
         required: true,
     },
+    createdAt: {
+        type:Date,
+        default: Date.now
+    },
     userId : {
         type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true,
     }
